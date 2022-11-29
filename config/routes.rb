8e54a2e9c317e5fd725bcfee4ebe0e0115ce1452
muflih <muflimuflih306@gmail.com>
2022-11-29 23:50:56 +0530

    Added portfolio items via resources generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :portfolios
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
   resources :blogs
+  # root "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
